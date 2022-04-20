cd0df16d98839b33d6881710b6d0bd260a949616
Svahnen <svahn.jonny@gmail.com>
2022-04-20 13:08:31 +0200

    Add default constructor

--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,10 @@ public class Student implements Serializable {
     int id;
     String name;
 
+    public Student() {
+    } // important to have a default constructor (one that does not take any
+      // parameters) or Serializable may not work
+
     public Student(int id, String name) {
         this.id = id;
         this.name = name;
